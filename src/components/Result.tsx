7a8bfcee1e9343eaee73b7c23b95981c00e45b0e
Hugo-Farias <hg80rod@gmail.com>
2023-04-25 14:29:47 -0300

    date calculations completed

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -1,7 +1,7 @@
 import "./Result.scss";
 import { formData } from "../App";
 import React from "react";
-
+import { calcDate } from "../helper";
 interface prop {
   data: formData;
 }
@@ -9,16 +9,29 @@ interface prop {
 const Result: React.FC<prop> = function ({ data }) {
   const { day, month, year }: formData = data;
 
+  const newDate = { day: "--", month: "--", year: "--" };
+
+  if (!isNaN(+(day + month + year))) {
+    const date: Date = new Date(+year, month - 1, +day);
+    const currDate: Date = new Date();
+
+    const diffDate = calcDate(date, currDate);
+
+    newDate["year"] = diffDate.year + "";
+    newDate["month"] = diffDate.month + "";
+    newDate["day"] = diffDate.day + "";
+  }
+
   return (
     <div className="result">
       <h1>
-        <span className="number">{year}</span>years
+        <span className="number">{newDate.year}</span>years
       </h1>
       <h1>
-        <span className="number">{month}</span>months
+        <span className="number">{newDate.month}</span>months
       </h1>
       <h1>
-        <span className="number">{day}</span>days
+        <span className="number">{newDate.day}</span>days
       </h1>
     </div>
   );
--- src/helper.ts
+++ src/helper.ts
@@ -5,3 +5,37 @@ export const checkInvalid = function (
 ): boolean {
   return value > max || value < min;
 };
+
+export const calcDate = function (
+  date1: Date,
+  date2: Date
+): { day: number; month: number; year: number } {
+  /*
+   * calcDate() : Calculates the difference between two dates
+   */
+
+  //new date instance
+  const date1_time_stamp = new Date(date1).getTime();
+  const date2_time_stamp = new Date(date2).getTime();
+
+  let calc;
+
+  //Check which timestamp is greater
+  if (date1_time_stamp > date2_time_stamp) {
+    calc = new Date(date1_time_stamp - date2_time_stamp);
+  } else {
+    calc = new Date(date2_time_stamp - date1_time_stamp);
+  }
+
+  //Retrieve the date, month and year
+  const calcFormatTmp =
+    calc.getDate() + "-" + (calc.getMonth() + 1) + "-" + calc.getFullYear();
+  //Convert to an array and store
+  const calcFormat = calcFormatTmp.split("-");
+  //Subtract each member of our array from the default date
+  const days_passed = Number(Math.abs(+calcFormat[0]) - 1);
+  const months_passed = Number(Math.abs(+calcFormat[1]) - 1);
+  const years_passed = Number(Math.abs(+calcFormat[2]) - 1970);
+
+  return { day: days_passed, month: months_passed, year: years_passed };
+};
